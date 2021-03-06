5f10bd8add8d34b1884ed2589c986f5390089c15
suryadeepjaykar <jaykarsa111@gmail.com>
2021-03-06 18:44:56 +0530

    initial commit

---
+++ Android/app/src/main/java/com/suryasoft/samplestickerapp/EntryActivity.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.suryasoft.samplestickerapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.AdRequest;
+
+public class EntryActivity extends BaseActivity {
+    //private View imageView;
+    //private View progressBar;
+    private LoadListAsyncTask loadListAsyncTask;
+    public static InterstitialAd mInterstitialAd;
+
+    //splash screen
+//    public static int SPLASH_TIME_OUT=4000;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-1326433409409587/6637949988");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+        });
+//        new Handler().postDelayed(new Runnable() {
+//            @Override public void run() {
+//                Intent i = new Intent(EntryActivity.this, StickerPackListActivity.class);
+//                startActivity(i);
+//                finish(); } }, 6000);
+
+//        imageView = findViewById(R.id.imageView);
+//        loadListAsyncTask = new LoadListAsyncTask(this);
+//        loadListAsyncTask.execute();
+
+        overridePendingTransition(0, 0);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        loadListAsyncTask = new LoadListAsyncTask(this);
+        loadListAsyncTask.execute();
+
+
+    }
+
+
+    public static void showAd(){
+        if(mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        }else{
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        }
+    }
+
+    private void showStickerPack(ArrayList<StickerPack> stickerPackList) {
+        //progressBar.setVisibility(View.GONE);
+        if (stickerPackList.size() > 1) {
+            final Intent intent = new Intent(this, StickerPackListActivity.class);
+            intent.putParcelableArrayListExtra(StickerPackListActivity.EXTRA_STICKER_PACK_LIST_DATA, stickerPackList);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(0, 0);
+        } else {
+            final Intent intent = new Intent(this, StickerPackDetailsActivity.class);
+            intent.putExtra(StickerPackDetailsActivity.EXTRA_SHOW_UP_BUTTON, false);
+            intent.putExtra(StickerPackDetailsActivity.EXTRA_STICKER_PACK_DATA, stickerPackList.get(0));
+            startActivity(intent);
+            finish();
+            overridePendingTransition(0, 0);
+        }
+    }
+
+    private void showErrorMessage(String errorMessage) {
+        //progressBar.setVisibility(View.GONE);
+        Log.e("EntryActivity", "error fetching sticker packs, " + errorMessage);
+        final TextView errorMessageTV = findViewById(R.id.error_message);
+        errorMessageTV.setText(getString(R.string.error_message, errorMessage));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (loadListAsyncTask != null && !loadListAsyncTask.isCancelled()) {
+            loadListAsyncTask.cancel(true);
+        }
+    }
+
+    static class LoadListAsyncTask extends AsyncTask<Void, Void, Pair<String, ArrayList<StickerPack>>> {
+        private final WeakReference<EntryActivity> contextWeakReference;
+
+        LoadListAsyncTask(EntryActivity activity) {
+            this.contextWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Pair<String, ArrayList<StickerPack>> doInBackground(Void... voids) {
+            ArrayList<StickerPack> stickerPackList;
+            try {
+                final Context context = contextWeakReference.get();
+                if (context != null) {
+                    stickerPackList = StickerPackLoader.fetchStickerPacks(context);
+                    if (stickerPackList.size() == 0) {
+                        return new Pair<>("could not find any packs", null);
+                    }
+                    for (StickerPack stickerPack : stickerPackList) {
+                        StickerPackValidator.verifyStickerPackValidity(context, stickerPack);
+                    }
+                    return new Pair<>(null, stickerPackList);
+                } else {
+                    return new Pair<>("could not fetch sticker packs", null);
+                }
+            } catch (Exception e) {
+                Log.e("EntryActivity", "error fetching sticker packs", e);
+                return new Pair<>(e.getMessage(), null);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Pair<String, ArrayList<StickerPack>> stringListPair) {
+
+            final EntryActivity entryActivity = contextWeakReference.get();
+            if (entryActivity != null) {
+                if (stringListPair.first != null) {
+                    entryActivity.showErrorMessage(stringListPair.first);
+                } else {
+                    entryActivity.showStickerPack(stringListPair.second);
+                }
+            }
+        }
+    }
+}
